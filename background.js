05a3cd2e2735ecf0d53ac616145760f6fa01a6a8
8-yh-73fhg-732gtf43t34t4f65g8qn74g907rq <killerban98@gmail.com>
2024-07-13 22:51:18 +0300

    ✨ Demon Back Red Theme

--- background.js
+++ background.js
@@ -1,41 +1,41 @@
-const backgroundImages = [
-    'url("WImages/483ut08e3jtbgw.png")',
-    'url("WImages/483ut08e3jtbgw1.png")',
-];
+// const backgroundImages = [
+//     'url("WImages/483ut08e3jtbgw.png")',
+//     'url("WImages/483ut08e3jtbgw1.png")',
+// ];
 
-let intervalId;
+// let intervalId;
 
-function getNextBackgroundImage() {
-    const currentImageIndex = backgroundImages.indexOf(document.body.style.backgroundImage);
-    const nextImageIndex = (currentImageIndex + 1) % backgroundImages.length;
-    return backgroundImages[nextImageIndex];
-}
+// function getNextBackgroundImage() {
+//     const currentImageIndex = backgroundImages.indexOf(document.body.style.backgroundImage);
+//     const nextImageIndex = (currentImageIndex + 1) % backgroundImages.length;
+//     return backgroundImages[nextImageIndex];
+// }
 
-function updateBackgroundImage() {
-    const newImage = getNextBackgroundImage();
-    document.body.style.backgroundImage = newImage;
-    document.body.style.backgroundSize = 'cover';
-    document.body.style.backgroundRepeat = 'no-repeat';
-    document.body.style.backgroundPosition = 'center center';
+// function updateBackgroundImage() {
+//     const newImage = getNextBackgroundImage();
+//     document.body.style.backgroundImage = newImage;
+//     document.body.style.backgroundSize = 'cover';
+//     document.body.style.backgroundRepeat = 'no-repeat';
+//     document.body.style.backgroundPosition = 'center center';
 
-    const modalDiv = document.querySelector('.modal-window > div');
-    modalDiv.style.backgroundImage = newImage;
-    modalDiv.style.backgroundSize = 'cover';
-    modalDiv.style.backgroundPosition = 'center center';
+//     const modalDiv = document.querySelector('.modal-window > div');
+//     modalDiv.style.backgroundImage = newImage;
+//     modalDiv.style.backgroundSize = 'cover';
+//     modalDiv.style.backgroundPosition = 'center center';
 
-    console.log('%cBackground image updated:', 'color: blue; font-size: 16px; font-weight: bold;', newImage);
-}
+//     console.log('%cBackground image updated:', 'color: blue; font-size: 16px; font-weight: bold;', newImage);
+// }
 
-function startBackgroundRotation() {
-    updateBackgroundImage();
-    intervalId = setInterval(updateBackgroundImage, 60000); 
-}
+// function startBackgroundRotation() {
+//     updateBackgroundImage();
+//     intervalId = setInterval(updateBackgroundImage, 60000); 
+// }
 
-startBackgroundRotation();
+// startBackgroundRotation();
 
-document.addEventListener('visibilitychange', function() {
-    if (!document.hidden) {
-        clearInterval(intervalId);
-        startBackgroundRotation();
-    }
-});
+// document.addEventListener('visibilitychange', function() {
+//     if (!document.hidden) {
+//         clearInterval(intervalId);
+//         startBackgroundRotation();
+//     }
+// });
